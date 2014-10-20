64f95e1efabb8eebc4a606efa933969d5506fc8a
David Wu <wudayu@gmail.com>
2014-10-20 22:47:06 +0800

    project init

---
+++ src/com/wudayu/daf/activity/MainActivity.java
@@ -0,0 +1,21 @@
+package com.wudayu.daf.activity;
+
+import org.androidannotations.annotations.EActivity;
+
+import com.wudayu.daf.R;
+
+
+/**
+ *
+ * @author: Wu Dayu
+ * @En_Name: David Wu
+ * @E-mail: wudayu@gmail.com
+ * @Created Time: Oct 20, 2014, 10:02:04 PM
+ * @Description: David Wu created this file.
+ *
+ **/
+
+@EActivity(R.layout.activity_main)
+public class MainActivity extends BaseActivity {
+
+}
---
+++ src/com/wudayu/daf/constant/BroadcastActions.java
@@ -0,0 +1,20 @@
+package com.wudayu.daf.constant;
+
+/**
+ *
+ * @author: Wu Dayu
+ * @En_Name: David Wu
+ * @E-mail: wudayu@gmail.com
+ * @Created Time: Oct 20, 2014, 9:56:02 PM
+ * @Description: David Wu created this file.
+ *
+ **/
+
+public class BroadcastActions {
+
+	private static final String PKG = "cn.wudayu.daf_";
+
+	/** 关闭所有的Activity */
+	public static final String FINISH_ACTIVITY = PKG + "finish_activity";
+
+}
